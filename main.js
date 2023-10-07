2fe4626079ffa78acbbb621b68aec9684ef7018d
hillaryhosea <107868525+hillaryhosea@users.noreply.github.com>
2023-10-07 23:51:28 +0300

    new adjustment

--- main.js
+++ main.js
@@ -1,43 +1,33 @@
-const tabs = document.querySelectorAll("#features > nav a");
- const tabsContent = document.querySelectorAll("article > section ");
+
+
+
+// const tabs = document.querySelectorAll("#features > nav a");
+//  const tabsContent = document.querySelectorAll("article > section ");
  
-tabs.forEach((tab) => {
-  tab.addEventListener("click", (e) => {
-    e.preventDefault();
+// tabs.forEach((tab) => {
+//   tab.addEventListener("click", (e) => {
+//     e.preventDefault();
 
-    tabs.forEach((tab) => {
-      tab.classList.remove("is-selected");
-    });
-
-    tab.classList.add("is-selected");
-
-    tabsContent.forEach((section) => {
-      section.classList.remove("is-selected");
-    });
-
-    tabsContent.forEach(function (section) {
-      if (section.dataset.id === that.dataset.id) {
-        section.classList.add("is-selected");
-      }
-    });
-
-  //   const selectedTabId = tab.dataset.id;
-  //   const selectedTabContent = document.querySelector(`#features > article section[data-id="${selectedTabId}"]`);
-  //   selectedTabContent.classList.add("is-selected");
-  });
-});
-
-
-// $(function() {
-//     var tabs = ("#features > nav a");
-//     tabs.click(function(e){
-//         e.preventDefault();
-//         var that = $(this);
-//         tabs.removeClass("is-selected");
-//         that.addClass ("is-selected");
-//         tabsContent.removeClass("is-selected");
-//         tabsContent
-//          .filter ((i, tab) => $(tab).data("id") ===that.data("id"))
-//          .addClass("is-selected");
+//     tabs.forEach((tab) => {
+//       tab.classList.remove("is-selected");
 //     });
+
+//     tab.classList.add("is-selected");
+
+//     tabsContent.forEach((section) => {
+//       section.classList.remove("is-selected");
+//     // });
+
+//     tabsContent.forEach(function (section) {
+//       if (section.dataset.id === that.dataset.id) {
+//         section.classList.add("is-selected");
+//       }
+//     });
+
+//     const selectedTabId = tab.dataset.id;
+//     const selectedTabContent = document.querySelector(`#features > article section[data-id="${selectedTabId}"]`);
+//     selectedTabContent.classList.add("is-selected");
+//   });
 // });
+
+
